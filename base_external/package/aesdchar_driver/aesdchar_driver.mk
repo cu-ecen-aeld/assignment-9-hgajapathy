dae80b2316e1e612670bb843a4a4f2ffa0b3905b
Harinarayanan Gajapathy <hgajapat@gmail.com>
2023-03-19 16:35:41 +0000

    Assignment 8 Updates
    updated commit hash for aesdchar driver

--- base_external/package/aesdchar_driver/aesdchar_driver.mk
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -3,7 +3,7 @@
 # AESDCHAR_DRIVER
 #
 ##############################################################
-AESDCHAR_DRIVER_VERSION = 'f170516efd872c7f5aa47b5bfc3f22e64a8232fc'
+AESDCHAR_DRIVER_VERSION = '3e02341e033badd88373089bed0ef6c394443fbb'
 AESDCHAR_DRIVER_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-hgajapathy.git'
 AESDCHAR_DRIVER_SITE_METHOD = git
 AESDCHAR_DRIVER_GIT_SUBMODULES = YES
