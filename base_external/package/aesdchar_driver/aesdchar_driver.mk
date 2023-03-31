7d36b959284b1de330bcba83ae4ad041787d3ec8
Harinarayanan Gajapathy <hgajapat@gmail.com>
2023-03-31 03:24:10 +0000

    Assignment 9 Updates
    missing MODULE_SUBDIRS flag in .mk file

--- base_external/package/aesdchar_driver/aesdchar_driver.mk
+++ base_external/package/aesdchar_driver/aesdchar_driver.mk
@@ -11,7 +11,7 @@ AESDCHAR_DRIVER_GIT_SUBMODULES = YES
 # for local build
 # AESDCHAR_DRIVER_SITE = '/home/haga9942/assignments-3-and-later'
 # AESDCHAR_DRIVER_SITE_METHOD = local
-# AESDCHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
+AESDCHAR_DRIVER_MODULE_SUBDIRS = aesd-char-driver
 
 define AESDCHAR_DRIVER_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/aesd-char-driver/aesdchar_load $(TARGET_DIR)/usr/bin
